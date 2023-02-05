f5ecaec3c9a3015e6114debee10da2a10b81d2e1
nhatmicls <cptprice123@outlook.com>
2023-02-06 02:10:18 +0700

    Change to -R 777

---
+++ cogs/command/modules/database.py
@@ -0,0 +1,19 @@
+import platform
+import requests
+
+import discord
+
+from discord.ext import commands
+
+from utils import *
+
+
+class botDatabase:
+    def __init__(self) -> None:
+        pass
+
+    async def add():
+        pass
+
+    async def get():
+        pass

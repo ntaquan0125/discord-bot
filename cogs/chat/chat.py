8bbfd13917f3ccd6e050846bd6492b09282aa553
Nguyen Quan <ntaquan0125@gmail.com>
2023-03-02 23:30:48 +0100

    Update gpt 3.5 model api

--- cogs/chat/chat.py
+++ cogs/chat/chat.py
@@ -9,8 +9,6 @@ from discord.ext import commands
 
 from utils import *
 
-from cogs.chat.modules.exceptions import NoApiKey
-
 
 # Set up the OpenAI API client
 openai.api_key = OPENAI_API_KEY
@@ -19,30 +17,22 @@ openai.api_key = OPENAI_API_KEY
 class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.model_engine = "text-davinci-003"
+        self.model_engine = 'gpt-3.5-turbo'
 
     def get_response(self, prompt):
-        completion = openai.Completion.create(
-            engine=self.model_engine,
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
+        completion = openai.ChatCompletion.create(
+            model=self.model_engine,
+            messages=[{"role": "user", "content": prompt}]
         )
-        generated_text = completion.choices[0].text.strip("\n")
+
+        generated_text = completion['choices'][0]['message']['content'].strip('\n')
         return generated_text
 
     @commands.command()
-    async def chat(self, ctx, *, arg):
-        try:
-            generated_text = self.get_response(arg)
-            await ctx.send(generated_text)
-        except Exception as e:
-            print(e)
+    async def turbo(self, ctx, *, arg):
+        generated_text = self.get_response(arg)
+        await ctx.send(generated_text)
 
 
 async def setup(bot):
-    if OPENAI_API_KEY == None:
-        raise NoApiKey("OpenAI")
     await bot.add_cog(Chat(bot))

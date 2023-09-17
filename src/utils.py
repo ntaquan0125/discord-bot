0fe8410b76b8b45389532c82aba494a55c07da9d
Micls <cptprice123@outlook.com>
2023-09-17 19:10:56 +0700

    [Micls] Add get config data

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from dotenv import load_dotenv
 
@@ -9,11 +10,23 @@ BOT_VERSION = 0.1
 # ffmpeg tool's path
 FFMPEG_PATH = "C:/Program Files/ffmpeg/bin/ffmpeg.exe"
 
+STATUS_FILE_PATH = "./config/bot_status.json"
+BIRTHDAY_WISH_PATH = "./config/birthday_wishes/"
+
+DEFAULT_CONFIG_FILE_PATH = "./config/default/default_config.json"
+
 
 def get_env_data(env: str):
     load_dotenv()
 
     return os.getenv(env, None)
+
+
+def get_config_value(config: str):
+    with open(DEFAULT_CONFIG_FILE_PATH) as default_config_file:
+        default_config_file_contents = json.loads(default_config_file.read())
+
+    return default_config_file_contents["default_config"][config]
 
 
 def print_debug(instance, info):
